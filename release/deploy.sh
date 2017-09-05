b7772ece4f2bc28ef36397c1a68621625eaa1971
Mihhail Lapushkin <mihhail.lapushkin@gmail.com>
2017-09-05 14:58:20 +0300

    Testing travis deployments.

--- release/deploy.sh
+++ release/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 openssl aes-256-cbc -K $encrypted_ee8fb316b8fd_key -iv $encrypted_ee8fb316b8fd_iv -in release/codesigning.asc.enc -out release/codesigning.asc -d
 gpg --fast-import release/codesigning.asc
