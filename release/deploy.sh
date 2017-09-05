fdb0772d5af10acadd4f84dcdba82fb31f909d6e
Mihhail Lapushkin <mihhail.lapushkin@gmail.com>
2017-09-05 15:34:35 +0300

    Fixing release.

--- release/deploy.sh
+++ release/deploy.sh
@@ -5,4 +5,4 @@ openssl aes-256-cbc -K $encrypted_ee8fb316b8fd_key -iv $encrypted_ee8fb316b8fd_i
 gpg --fast-import release/codesigning.asc
 
 ./mvnw org.codehaus.mojo:versions-maven-plugin:2.1:set -DgenerateBackupPoms=false -DnewVersion=$TRAVIS_TAG
-./mvnw deploy --settings release/settings.xml
+./mvnw deploy -P release --settings release/settings.xml
