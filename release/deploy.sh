9cf27816975e7f67b2252aedb108c82725c6970a
Mihhail Lapushkin <mihhail.lapushkin@gmail.com>
2017-12-28 19:47:26 +0200

    A bit of housekeeping.

--- release/deploy.sh
+++ release/deploy.sh
@@ -4,5 +4,5 @@ set -x
 openssl aes-256-cbc -K $encrypted_ee8fb316b8fd_key -iv $encrypted_ee8fb316b8fd_iv -in release/codesigning.asc.enc -out release/codesigning.asc -d
 gpg --fast-import release/codesigning.asc
 
-./mvnw org.codehaus.mojo:versions-maven-plugin:2.1:set -DgenerateBackupPoms=false -DnewVersion=$TRAVIS_TAG
+./mvnw versions:set -DnewVersion=$TRAVIS_TAG
 ./mvnw deploy -P release --settings release/settings.xml
